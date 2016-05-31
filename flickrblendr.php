54c2add07fcfe94d548d4aa694693d80c8279c8c
troutcolor <johnjohnston@mac.com>
2016-05-31 06:30:05 +0100

    fixing flush_rewrite_rules by adding register posts

--- flickrblendr.php
+++ flickrblendr.php
@@ -103,6 +103,8 @@ add_action( 'admin_menu', 'flickrblendr_plugin_menu' );
 		register_post_type( 'flickrblendr', $args );	
 	}
 	
+	
+	
 	add_action( 'init', 'my_custom_post_flickrblendr' );
 	
 	
@@ -167,6 +169,8 @@ add_action( 'wp_enqueue_scripts', 'flickrblendrpage_enqueue_scripts' );
 
 	function flickrblendr_activate() {
 	    // register taxonomies/post types here
+		
+		my_custom_post_flickrblendr();
 	    flush_rewrite_rules();
 	}
 
